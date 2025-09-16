ab78f9a1801e3cfe00205dda05303e5b982581ca
Soton Ahmed <soton371@gmail.com>
2025-09-16 22:43:36 +0600

    add cors

--- app/main.py
+++ app/main.py
@@ -2,14 +2,23 @@ from app.core.exception_handlers import add_exception_handlers
 from fastapi import FastAPI
 from app.api.v1.auth import auth_router
 from app.api.v1.leaderboard import leaderboard_router
+from fastapi.middleware.cors import CORSMiddleware
+from app.core.setting import settings
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 add_exception_handlers(app)
 
 app.include_router(auth_router.router)
 app.include_router(leaderboard_router.router)
-
 
 
 # source .venv/bin/activate
@@ -24,4 +33,3 @@ app.include_router(leaderboard_router.router)
 # alembic init alembic
 # uv run alembic revision --autogenerate -m "Initial migration"
 # uv run alembic upgrade head
-
